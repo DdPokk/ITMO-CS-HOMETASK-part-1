7864376730985ee55c947a90e5fdc603a8f728e3
DdPokk <9111322054@mail.ru>
2021-11-09 11:44:34 +0300

    Добавьте файлы проекта.

---
+++ Project1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;//число до которого будем находить простые числа
+            N = int.Parse(Console.ReadLine());//вводим N
+            for (int i = 2; i <= N; i++)
+            {
+                if (isSimple(i))
+                {
+                    Console.Write(i.ToString() + ",");
+                }
+            }
+        }
+        //метод который определяет простое число или нет
+        private static bool isSimple(int N)
+        {
+            //чтоб убедится простое число или нет достаточно проверить не делитсья ли 
+            //число на числа до его половины
+            for (int i = 2; i < (int)(N / 2); i++)
+            {
+                if (N % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
